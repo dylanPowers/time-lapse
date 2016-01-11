885120134002660b1df50667350523b72a5fb233
Dylan Powers <dylan.kyle.powers@gmail.com>
2016-01-11 11:54:47 -0800

    Removed the autogenerated crap and implemented fullscreen how I want

--- app/src/main/java/com/dylankpowers/timelapse/Camera.java
+++ app/src/main/java/com/dylankpowers/timelapse/Camera.java
@@ -36,71 +36,6 @@ import java.util.List;
 public class Camera extends AppCompatActivity {
   private static final String TAG = "Time Lapse";
 
-  /**
-   * Whether or not the system UI should be auto-hidden after
-   * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-   */
-  private static final boolean AUTO_HIDE = true;
-
-  /**
-   * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-   * user interaction before hiding the system UI.
-   */
-  private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-  /**
-   * Some older devices needs a small delay between UI widget updates
-   * and a change of the status and navigation bar.
-   */
-  private static final int UI_ANIMATION_DELAY = 300;
-  private final Handler mHideHandler = new Handler();
-//  private View mContentView;
-  private final Runnable mHidePart2Runnable = new Runnable() {
-    @SuppressLint("InlinedApi")
-    @Override
-    public void run() {
-      // Delayed removal of status and navigation bar
-
-      // Note that some of these constants are new as of API 16 (Jelly Bean)
-      // and API 19 (KitKat). It is safe to use them, as they are inlined
-      // at compile-time and do nothing on earlier devices.
-      mTextureView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
-          | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-    }
-  };
-  private View mControlsView;
-  private final Runnable mShowPart2Runnable = new Runnable() {
-    @Override
-    public void run() {
-      // Delayed display of UI elements
-      ActionBar actionBar = getSupportActionBar();
-      if (actionBar != null) {
-        actionBar.show();
-      }
-      mControlsView.setVisibility(View.VISIBLE);
-    }
-  };
-  private boolean mVisible;
-  private final Runnable mHideRunnable = new Runnable() {
-    @Override
-    public void run() {
-      hide();
-    }
-  };
-  /**
-   * Touch listener to use for in-layout UI controls to delay hiding the
-   * system UI. This is to prevent the jarring behavior of controls going away
-   * while interacting with activity UI.
-   */
-  private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-      if (AUTO_HIDE) {
-        delayedHide(AUTO_HIDE_DELAY_MILLIS);
-      }
-      return false;
-    }
-  };
 
   private CameraPreviewSurfaceListener mSurfaceTextureListener = new CameraPreviewSurfaceListener();
   private TextureView mTextureView;
@@ -111,25 +46,17 @@ public class Camera extends AppCompatActivity {
 
     setContentView(R.layout.activity_camera);
 
-    mVisible = true;
-    mControlsView = findViewById(R.id.fullscreen_content_controls);
     mTextureView = (TextureView) findViewById(R.id.camera_preview);
+  }
 
-
-    // Set up the user interaction to manually show or hide the system UI.
-    mTextureView.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        toggle();
-      }
-    });
-
-
-    // Upon interacting with UI controls, delay any scheduled hide()
-    // operations to prevent the jarring behavior of controls going away
-    // while interacting with the UI.
-    findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    if (hasFocus) {
+      mTextureView.setSystemUiVisibility(
+          View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+          | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+          | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
   }
 
   private void doCameraThangs() {
@@ -160,7 +87,6 @@ public class Camera extends AppCompatActivity {
         Log.d(TAG, "Back camera");
         if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
           try {
-            final AppCompatActivity that = this;
             manager.openCamera(cameraId, new CameraDevice.StateCallback() {
               @Override
               public void onOpened(CameraDevice camera) {
@@ -192,7 +118,7 @@ public class Camera extends AppCompatActivity {
 
                         @Override
                         public void onConfigureFailed(CameraCaptureSession session) {
-                          Log.d(TAG, "Configure failed. We suck dicks");
+                          Log.d(TAG, "Configure failed. We suck");
                         }
                       }, null);
                 } catch (CameraAccessException e) {
@@ -226,11 +152,6 @@ public class Camera extends AppCompatActivity {
   @Override
   protected void onPostCreate(Bundle savedInstanceState) {
     super.onPostCreate(savedInstanceState);
-
-    // Trigger the initial hide() shortly after the activity has been
-    // created, to briefly hint to the user that UI controls
-    // are available.
-    delayedHide(100);
   }
 
   @Override
@@ -258,48 +179,6 @@ public class Camera extends AppCompatActivity {
     }
   }
 
-  private void toggle() {
-    if (mVisible) {
-      hide();
-    } else {
-      show();
-    }
-  }
-
-  private void hide() {
-    // Hide UI first
-    ActionBar actionBar = getSupportActionBar();
-    if (actionBar != null) {
-      actionBar.hide();
-    }
-    mControlsView.setVisibility(View.GONE);
-    mVisible = false;
-
-    // Schedule a runnable to remove the status and navigation bar after a delay
-    mHideHandler.removeCallbacks(mShowPart2Runnable);
-    mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-  }
-
-  @SuppressLint("InlinedApi")
-  private void show() {
-    // Show the system bar
-    mTextureView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-    mVisible = true;
-
-    // Schedule a runnable to display UI elements after a delay
-    mHideHandler.removeCallbacks(mHidePart2Runnable);
-    mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-  }
-
-  /**
-   * Schedules a call to hide() in [delay] milliseconds, canceling any
-   * previously scheduled calls.
-   */
-  private void delayedHide(int delayMillis) {
-    mHideHandler.removeCallbacks(mHideRunnable);
-    mHideHandler.postDelayed(mHideRunnable, delayMillis);
-  }
-
   class CameraPreviewSurfaceListener implements TextureView.SurfaceTextureListener {
 
     @Override
@@ -325,22 +204,16 @@ public class Camera extends AppCompatActivity {
   private CameraCaptureSession.CaptureCallback mCaptureCallback
       = new CameraCaptureSession.CaptureCallback() {
 
-    private void process(CaptureResult result) {
-      Log.d(TAG, "processing....");
-    }
-
     @Override
     public void onCaptureProgressed(@NonNull CameraCaptureSession session,
                                     @NonNull CaptureRequest request,
                                     @NonNull CaptureResult partialResult) {
-      process(partialResult);
     }
 
     @Override
     public void onCaptureCompleted(@NonNull CameraCaptureSession session,
                                    @NonNull CaptureRequest request,
                                    @NonNull TotalCaptureResult result) {
-      process(result);
     }
 
   };
