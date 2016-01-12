aa14e431508e26b0fa9f993fdee0b2ea678617ef
Dylan Powers <dylan.kyle.powers@gmail.com>
2016-01-11 17:02:52 -0800

    Preview rotation works correctly

--- app/src/main/java/com/dylankpowers/timelapse/Camera.java
+++ app/src/main/java/com/dylankpowers/timelapse/Camera.java
@@ -1,9 +1,11 @@
 package com.dylankpowers.timelapse;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -15,19 +17,15 @@ import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.CaptureResult;
 import android.hardware.camera2.TotalCaptureResult;
 import android.support.annotation.NonNull;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.Surface;
-import android.view.SurfaceView;
 import android.view.TextureView;
 import android.view.View;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -37,8 +35,92 @@ public class Camera extends AppCompatActivity {
   private static final String TAG = "Time Lapse";
 
 
-  private CameraPreviewSurfaceListener mSurfaceTextureListener = new CameraPreviewSurfaceListener();
-  private TextureView mTextureView;
+  private final TextureView.SurfaceTextureListener
+      mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture texture, int width, int height) {
+      doCameraThangs();
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture texture, int width, int height) {
+      int rotation = getWindowManager().getDefaultDisplay().getRotation();
+      Matrix matrix = new Matrix();
+      RectF viewRect = new RectF(0, 0, width, height);
+      float centerX = viewRect.centerX();
+      float centerY = viewRect.centerY();
+      if (Surface.ROTATION_90 == rotation || Surface.ROTATION_270 == rotation) {
+        matrix.postRotate(90 * (rotation - 2), centerX, centerY);
+        matrix.postScale(width / (float) height, 1.0f, centerX, centerY);
+      } else if (Surface.ROTATION_180 == rotation) {
+          matrix.postRotate(180, centerX, centerY);
+      }
+      mPreviewView.setTransform(matrix);
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture texture) {
+      return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture texture) {
+    }
+
+  };
+
+  private final CameraDevice.StateCallback mCameraStateCallback = new CameraDevice.StateCallback() {
+    @Override
+    public void onOpened(CameraDevice camera) {
+      Log.d(TAG, "Camera Opened");
+      try {
+        final CameraDevice finalCamera = camera;
+        final Surface surface = new Surface(mPreviewView.getSurfaceTexture());
+        camera.createCaptureSession(Arrays.asList(surface),
+            new CameraCaptureSession.StateCallback() {
+
+              @Override
+              public void onConfigured(CameraCaptureSession session) {
+                Log.d(TAG, "configured");
+                CaptureRequest.Builder previewRequestBuilder;
+                try {
+                  previewRequestBuilder = finalCamera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                } catch (CameraAccessException e) {
+                  e.printStackTrace();
+                  return;
+                }
+
+                previewRequestBuilder.addTarget(surface);
+                try {
+                  session.setRepeatingRequest(previewRequestBuilder.build(), mCaptureCallback, new Handler(getMainLooper()));
+                } catch (CameraAccessException e) {
+                  e.printStackTrace();
+                }
+              }
+
+              @Override
+              public void onConfigureFailed(CameraCaptureSession session) {
+                Log.d(TAG, "Configure failed. We suck");
+              }
+            }, null);
+      } catch (CameraAccessException e) {
+        Log.e(TAG, "Thingys go boom!", e);
+      }
+    }
+
+    @Override
+    public void onDisconnected(CameraDevice camera) {
+      Log.d(TAG, "Camera Disconnected");
+    }
+
+    @Override
+    public void onError(CameraDevice camera, int error) {
+      Log.d(TAG, "Camera Error: " + error);
+    }
+  };
+
+  private TextureView mPreviewView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +128,13 @@ public class Camera extends AppCompatActivity {
 
     setContentView(R.layout.activity_camera);
 
-    mTextureView = (TextureView) findViewById(R.id.camera_preview);
+    mPreviewView = (TextureView) findViewById(R.id.camera_preview);
   }
 
   @Override
   public void onWindowFocusChanged(boolean hasFocus) {
     if (hasFocus) {
-      mTextureView.setSystemUiVisibility(
+      mPreviewView.setSystemUiVisibility(
           View.SYSTEM_UI_FLAG_LAYOUT_STABLE
           | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
           | View.SYSTEM_UI_FLAG_FULLSCREEN);
@@ -87,55 +169,7 @@ public class Camera extends AppCompatActivity {
         Log.d(TAG, "Back camera");
         if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
           try {
-            manager.openCamera(cameraId, new CameraDevice.StateCallback() {
-              @Override
-              public void onOpened(CameraDevice camera) {
-                Log.d(TAG, "Camera Opened");
-                try {
-                  final CameraDevice finalCamera = camera;
-                  final Surface surface = new Surface(mTextureView.getSurfaceTexture());
-                  camera.createCaptureSession(Arrays.asList(surface),
-                      new CameraCaptureSession.StateCallback() {
-
-                        @Override
-                        public void onConfigured(CameraCaptureSession session) {
-                          Log.d(TAG, "configured");
-                          CaptureRequest.Builder previewRequestBuilder;
-                          try {
-                            previewRequestBuilder = finalCamera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-                          } catch (CameraAccessException e) {
-                            e.printStackTrace();
-                            return;
-                          }
-
-                          previewRequestBuilder.addTarget(surface);
-                          try {
-                            session.setRepeatingRequest(previewRequestBuilder.build(), mCaptureCallback, handler);
-                          } catch (CameraAccessException e) {
-                            e.printStackTrace();
-                          }
-                        }
-
-                        @Override
-                        public void onConfigureFailed(CameraCaptureSession session) {
-                          Log.d(TAG, "Configure failed. We suck");
-                        }
-                      }, null);
-                } catch (CameraAccessException e) {
-                  Log.e(TAG, "Thingys go boom!", e);
-                }
-              }
-
-              @Override
-              public void onDisconnected(CameraDevice camera) {
-                Log.d(TAG, "Camera Disconnected");
-              }
-
-              @Override
-              public void onError(CameraDevice camera, int error) {
-                Log.d(TAG, "Camera Error: " + error);
-              }
-            }, handler);
+            manager.openCamera(cameraId, mCameraStateCallback, handler);
           } catch (CameraAccessException e) {
             Log.e(TAG, "Errors n such", e);
           }
@@ -158,10 +192,10 @@ public class Camera extends AppCompatActivity {
   protected void onResume() {
     super.onResume();
 
-    if (mTextureView.isAvailable()) {
+    if (mPreviewView.isAvailable()) {
       doCameraThangs();
     } else {
-      mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+      mPreviewView.setSurfaceTextureListener(mSurfaceTextureListener);
     }
   }
 
@@ -178,28 +212,6 @@ public class Camera extends AppCompatActivity {
       Log.d(TAG, "Permission not granted :(((( " + grantResults[0]);
     }
   }
-
-  class CameraPreviewSurfaceListener implements TextureView.SurfaceTextureListener {
-
-    @Override
-    public void onSurfaceTextureAvailable(SurfaceTexture texture, int width, int height) {
-      doCameraThangs();
-    }
-
-    @Override
-    public void onSurfaceTextureSizeChanged(SurfaceTexture texture, int width, int height) {
-    }
-
-    @Override
-    public boolean onSurfaceTextureDestroyed(SurfaceTexture texture) {
-      return true;
-    }
-
-    @Override
-    public void onSurfaceTextureUpdated(SurfaceTexture texture) {
-    }
-
-  };
 
   private CameraCaptureSession.CaptureCallback mCaptureCallback
       = new CameraCaptureSession.CaptureCallback() {
